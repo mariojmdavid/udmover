bd025433f984e95eb99c5e4f834bb963c8777259
mariojmdavid <mariojmdavid@gmail.com>
2018-08-17 09:59:05 +0100

    fix version and name to init

---
+++ udmover/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+name = 'udmover'
+__version__ = "0.0.1.dev1"
--- udmover/udmover.py
+++ udmover/udmover.py
@@ -11,12 +11,10 @@ import udmover
 from cli.cmdparser import CmdParser
 from cli.cmd import Cmd
 
-name = 'udmover'
 __author__ = "Mario David"
 __copyright__ = "LIP"
 __license__ = "Licensed under the Apache License, Version 2.0"
 __date__ = "2018"
-__version__ = "0.0.1.dev1"
 
 _logger = logging.getLogger(__name__)
 
